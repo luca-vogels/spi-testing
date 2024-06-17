940d855f25e9134b1d9feb91189361914ba0b2db
Luca Vogels <github@luca-vogels.com>
2024-06-17 21:13:11 +0200

    Added condition_variable benchmark to MutexBenchmark

--- src/MutexBenchmark.cpp
+++ src/MutexBenchmark.cpp
@@ -1,6 +1,8 @@
 #include <chrono>
 #include <cstdint>
 #include <iostream>
+
+#include <condition_variable>
 #include <mutex>
 #include <shared_mutex>
 
@@ -9,6 +11,8 @@ int main(){
     const uint64_t ITERATIONS = 50000000;
     std::mutex mutex;
     std::shared_mutex sharedMutex;
+    std::condition_variable conditionVariable;
+
 
     // mutex::lock_guard():             ~ 42.4 Mio/sec
     auto startTime = std::chrono::high_resolution_clock::now();
@@ -49,6 +53,23 @@ int main(){
     endTime = std::chrono::high_resolution_clock::now();
     std::cout << "shared_mutex::shared_lock(): " << (ITERATIONS * 1000000) / std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count() << "/s" << std::endl;
 
+
+
+    // condition_variable::notify_one():     ~  235.3 Mio/sec
+    startTime = std::chrono::high_resolution_clock::now();
+    for(uint64_t i=0; i < ITERATIONS; i++){
+        conditionVariable.notify_one();
+    }
+    endTime = std::chrono::high_resolution_clock::now();
+    std::cout << "condition_variable::notify_one(): " << (ITERATIONS * 1000000) / std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count() << "/s" << std::endl;
+
+    // condition_variable::notify_all():     ~  258.1 Mio/sec   <-- ALWAYS WINNER over notify_one()
+    startTime = std::chrono::high_resolution_clock::now();
+    for(uint64_t i=0; i < ITERATIONS; i++){
+        conditionVariable.notify_all();
+    }
+    endTime = std::chrono::high_resolution_clock::now();
+    std::cout << "condition_variable::notify_all(): " << (ITERATIONS * 1000000) / std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count() << "/s" << std::endl;
 
     return 0;
 }
